a3f0f965c8bd5252064920c5eaaec1b8db600aed
AndryGamerTV <asdaartefret@gmail.com>
2025-01-15 23:05:09 +0200

    add complete or not to buttons

---
+++ site/RightFalse.js
@@ -0,0 +1,56 @@
+function toggleState(button) {
+      if (!button.classList.contains('green') && !button.classList.contains('red')) {
+          button.classList.add('green');
+          button.textContent = '1. Do the homework ✔';
+      } else if (button.classList.contains('green')) {
+          button.classList.remove('green');
+          button.classList.add('red');
+          button.textContent = '1. Do the homework ✖';
+      } else if (button.classList.contains('red')) {
+          button.classList.remove('red');
+          button.classList.add('green');
+          button.textContent = '1. Do the homework ✔';
+      }
+  }
+  function toggleState1(button) {
+    if (!button.classList.contains('green') && !button.classList.contains('red')) {
+        button.classList.add('green');
+        button.textContent = '2. Do the dinner ✔';
+    } else if (button.classList.contains('green')) {
+        button.classList.remove('green');
+        button.classList.add('red');
+        button.textContent = '2. Do the dinner ✖';
+    } else if (button.classList.contains('red')) {
+        button.classList.remove('red');
+        button.classList.add('green');
+        button.textContent = '2. Do the dinner ✔';
+    }
+}
+function toggleState2(button) {
+    if (!button.classList.contains('green') && !button.classList.contains('red')) {
+        button.classList.add('green');
+        button.textContent = '3. Go work ✔';
+    } else if (button.classList.contains('green')) {
+        button.classList.remove('green');
+        button.classList.add('red');
+        button.textContent = '3. Go work ✖';
+    } else if (button.classList.contains('red')) {
+        button.classList.remove('red');
+        button.classList.add('green');
+        button.textContent = '3. Go work ✔';
+    }
+}
+function toggleState3(button) {
+    if (!button.classList.contains('green') && !button.classList.contains('red')) {
+        button.classList.add('green');
+        button.textContent = '4. Go to sleep ✔';
+    } else if (button.classList.contains('green')) {
+        button.classList.remove('green');
+        button.classList.add('red');
+        button.textContent = '4. Go to sleep ✖';
+    } else if (button.classList.contains('red')) {
+        button.classList.remove('red');
+        button.classList.add('green');
+        button.textContent = '4. Go to sleep ✔';
+    }
+}
