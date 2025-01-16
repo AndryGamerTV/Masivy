3286a46d3f950ddc57fc589d42735163f54de349
AndryGamerTV <asdaartefret@gmail.com>
2025-01-16 14:35:08 +0200

    update

--- testing/Operations_with_arrays.js
+++
@@ -1,7 +0,0 @@
-const pp = console.log;
-let style = ["jazz", "blues"];pp(style);
-
-style.push("Roch-n-Roll");pp(style);
-style.splice(1, 1, 'Classics');pp(style);
-style.splice(0, 1);pp(style);
-style.unshift('Rap', 'Reggae');pp(style);
--- testing/n.js
+++
@@ -1,5 +0,0 @@
-const log = console.log;
-log("helo");log(1 
-    + 3
-    -4
-    *4);log(2/2);
