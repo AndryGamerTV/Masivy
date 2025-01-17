7a7c536bad3e00253d4e53dae77c9b5f511cb114
AndryGamerTV <asdaartefret@gmail.com>
2025-01-17 07:57:42 +0200

    text and button update

--- site/RightFalse.js
+++ site/RightFalse.js
@@ -1,75 +1,93 @@
 function toggleState(button) {
     if (!button.classList.contains('green') && !button.classList.contains('red') && !button.classList.contains('white')) {
         button.classList.add('green');
-        button.textContent = '1. Do the homework ✔';
+        button.textContent = '1. Make breakfast and drink ✔';
     } else if (button.classList.contains('green')) {
         button.classList.remove('green');
         button.classList.add('red');
-        button.textContent = '1. Do the homework ✖';
+        button.textContent = '1. Make breakfast and drink ✖';
     } else if (button.classList.contains('red')) {
         button.classList.remove('red');
         button.classList.add('white');
-        button.textContent = '1. Do the homework';
+        button.textContent = '1. Make breakfast and drink';
     } else if (button.classList.contains('white')) {
         button.classList.remove('white');
         button.classList.add('green');
-        button.textContent = '1. Do the homework ✔';
+        button.textContent = '1. Make breakfast and drink ✔';
     }
 }
 
 function toggleState1(button) {
     if (!button.classList.contains('green') && !button.classList.contains('red') && !button.classList.contains('white')) {
         button.classList.add('green');
-        button.textContent = '2. Do the dinner ✔';
+        button.textContent = '2. Focus on the main task or ptoject ✔';
     } else if (button.classList.contains('green')) {
         button.classList.remove('green');
         button.classList.add('red');
-        button.textContent = '2. Do the dinner ✖';
+        button.textContent = '2. Focus on the main task or ptoject ✖';
     } else if (button.classList.contains('red')) {
         button.classList.remove('red');
         button.classList.add('white');
-        button.textContent = '2. Do the dinner';
+        button.textContent = '2. Focus on the main task or ptoject';
     } else if (button.classList.contains('white')) {
         button.classList.remove('white');
         button.classList.add('green');
-        button.textContent = '2. Do the dinner ✔';
+        button.textContent = '2. Focus on the main task or ptoject ✔';
     }
 }
 
 function toggleState2(button) {
     if (!button.classList.contains('green') && !button.classList.contains('red') && !button.classList.contains('white')) {
         button.classList.add('green');
-        button.textContent = '3. Go work ✔';
+        button.textContent = '3. Spend time on the hobby ✔';
     } else if (button.classList.contains('green')) {
         button.classList.remove('green');
         button.classList.add('red');
-        button.textContent = '3. Go work ✖';
+        button.textContent = '3. Spend time on the hobby ✖';
     } else if (button.classList.contains('red')) {
         button.classList.remove('red');
         button.classList.add('white');
-        button.textContent = '3. Go work';
+        button.textContent = '3. Spend time on the hobby';
     } else if (button.classList.contains('white')) {
         button.classList.remove('white');
         button.classList.add('green');
-        button.textContent = '3. Go work ✔';
+        button.textContent = '3. Spend time on the hobby ✔';
     }
 }
 
 function toggleState3(button) {
     if (!button.classList.contains('green') && !button.classList.contains('red') && !button.classList.contains('white')) {
         button.classList.add('green');
-        button.textContent = '4. Go to sleep ✔';
+        button.textContent = '4. Prepare a plan for the next day ✔';
     } else if (button.classList.contains('green')) {
         button.classList.remove('green');
         button.classList.add('red');
-        button.textContent = '4. Go to sleep ✖';
+        button.textContent = '4. Prepare a plan for the next day ✖';
     } else if (button.classList.contains('red')) {
         button.classList.remove('red');
         button.classList.add('white');
-        button.textContent = '4. Go to sleep';
+        button.textContent = '4. Prepare a plan for the next day';
     } else if (button.classList.contains('white')) {
         button.classList.remove('white');
         button.classList.add('green');
-        button.textContent = '4. Go to sleep ✔';
+        button.textContent = '4. Prepare a plan for the next day ✔';
+    }
+}
+function toggleState4(button) {
+    if (!button.classList.contains('green') && !button.classList.contains('red') && !button.classList.contains('white')) {
+        button.classList.add('green');
+        button.textContent = '5. Go walk or enjoy a favorite movie ✔';
+    } else if (button.classList.contains('green')) {
+        button.classList.remove('green');
+        button.classList.add('red');
+        button.textContent = '5. Go walk or enjoy a favorite movie ✖';
+    } else if (button.classList.contains('red')) {
+        button.classList.remove('red');
+        button.classList.add('white');
+        button.textContent = '5. Go walk or enjoy a favorite movie';
+    } else if (button.classList.contains('white')) {
+        button.classList.remove('white');
+        button.classList.add('green');
+        button.textContent = '5. Go walk or enjoy a favorite movie ✔';
     }
 }
